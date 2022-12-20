c767d78889b0e5192c04f5bd065aa8c768bafe2f
Pobitrodeb <pobitrodn@gmail.com>
2022-12-20 06:02:36 +0600

    only view file ready

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-    <title>CRUD with Ajax </title>
+    <title> CRUD with Ajax </title>
   </head>
   <body>
 
@@ -15,7 +15,7 @@
         <div class="container">
 
         <div class="card">
-                    <div class="card-header text-center bg-primary text-white"> LARAVEL CRUD with Ajax </div>
+                    <div class="card-header text-center bg-primary text-white py-3"> LARAVEL CRUD with Ajax </div>
                     <a href=""class="btn btn-success m-3 w-25" data-bs-toggle="modal" data-bs-target="#addeModal"> Add Product </a>
                     <div class="card-body">
                         <table class="table table-bordered">
@@ -44,8 +44,8 @@
         </div>
   </section>
   @include('home.modal');
-  
-   
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
    <script>
